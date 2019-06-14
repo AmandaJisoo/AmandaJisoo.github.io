76a6f5d99aaa64335fd14e99f02c990a28bba539
Amanda Park <wd1204@uw.edu>
2019-06-13 20:52:26 -0700

    another change

--- imageSlide.js
+++ imageSlide.js
@@ -7,7 +7,7 @@ function showNext() {
   var nextIndex = (currentIndex + 1) % slideCount; //next images
   console.log(nextIndex);
   slides.eq(currentIndex).fadeOut(800); // removing current images
-  slides.eq(nextIndex).fadeIn(800); // appering next image 
+  slides.eq(nextIndex).fadeIn(800); // appering next image
   currentIndex = nextIndex; /
 }
 setInterval(showNext, 3000);
