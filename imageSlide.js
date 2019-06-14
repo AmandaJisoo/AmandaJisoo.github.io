381d031c18bc125157d5438355a778e2235fc290
Amanda Park <wd1204@uw.edu>
2019-06-13 20:24:36 -0700

    js file added

--- imageSlide.js
+++ imageSlide.js
@@ -5,8 +5,9 @@ var slideCount = slides.length;
 
 function showNext() {
   var nextIndex = (currentIndex + 1) % slideCount; //next images
+  console.log(nextIndex);
   slides.eq(currentIndex).fadeOut(800); // removing current images
-  slides.eq(nextIndex).fadein(800); // appering next image
-  currentindex = nextindex;
+  slides.eq(nextIndex).fadeIn(800); // appering next image 
+  currentIndex = nextIndex; /
 }
 setInterval(showNext, 3000);
